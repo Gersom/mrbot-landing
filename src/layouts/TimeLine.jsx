72fb98116bd6067c53f4e2a093ffa66e90f9ef92
unapc <oficina@slonpay.com>
2023-04-20 11:31:26 -0500

    Add responsive in TimeLine section

--- src/layouts/TimeLine.jsx
+++ src/layouts/TimeLine.jsx
@@ -4,11 +4,13 @@ import { GerTimeline } from "../components/GerTimeline";
 export const TimeLine = () => {
   return (
     <div className="time-line">
-      <h2 className="time-line__title">
-        Nuestra misión a largo plazo
-      </h2>
-      <div className="time-line__content">
-        <GerTimeline />
+      <div className="container">
+        <h2 className="time-line__title">
+          Nuestra misión a largo plazo
+        </h2>
+        <div className="time-line__content">
+          <GerTimeline />
+        </div>
       </div>
     </div>
   )
